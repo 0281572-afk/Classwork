e1c0dcb1e40155e32717b8169764730e836a5615
0281572-afk <0281572@up.edu.mx>
2026-09-04 20:18:07 +0000

    pigLatin

--- Portfolio/05-JS/js/pig_latin.js
+++ Portfolio/05-JS/js/pig_latin.js
@@ -1,30 +1,51 @@
 /*
 Pig Latin
 */
+//move first consotant to the end, add ay after
+//move first vowel to the end, add way after
 
 function igpayAtinlay(str) {
+  if (!str) return "";
+
   // TODO: Initialize the word array properly
-  var returnArray = [],
-    wordArray = [];
+  var vowels = "aeiou";
+  var returnArray = [];
+  var wordArray = str.split(" ");
+
   // TODO: make sure that the output is being properly built to produce the desired result.
   for (var i = 0; i < wordArray.length; i++) {
     var word = wordArray[i];
     var beginning = word.charAt(0);
 
-    if (/[aeiouAEIOU]/.test(beginning)) {
-      returnArray.push(word);
+    if (vowels.includes(beginning.toLowerCase())) { 
+      returnArray.push(word + "way");
       continue;
     }
 
     for (var ii = 1; ii < word.length; ii++) {
-      if (/[aeiouAEIOU]/.test(word.charAt(ii))) {
+      if (vowels.includes(word.charAt(ii).toLowerCase())) {
         break;
       } else {
         beginning += word.charAt(ii);
       }
     }
+
+    var restOfWord = word.slice(beginning.length);
+    returnArray.push(restOfWord + beginning + "ay");
   }
+
   return returnArray.join(" ");
+}
+
+//for button
+function translated(){
+  "use strict";
+  var inputVal = document.getElementById("txtVal").value;
+  var spanLabel = document.getElementById("pigLatLbl");
+  
+  var translated = igpayAtinlay(inputVal);
+  spanLabel.innerText = translated;
+
 }
 
 // Some examples of expected outputs
